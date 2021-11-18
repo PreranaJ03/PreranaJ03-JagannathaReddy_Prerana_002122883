f9bc4506ee040699b00aa4465cef60d2cc602ba9
Prerana <jagannathareddy.p@northeastern.edu>
2021-11-18 19:22:29 -0400

    Restaurant package changes

--- db4odemo/src/Business/Restaurant/Dishes.java
+++ db4odemo/src/Business/Restaurant/Dishes.java
@@ -11,4 +11,43 @@ package Business.Restaurant;
  */
 public class Dishes {
     
+    private String name;
+    private String description;
+    private String price;
+
+    public Dishes(String name, String description, String price) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+    
+    @Override
+    public String toString() {
+        return name;
+    }
+    
 }
